fb3a8f21ba77c8f403c0132e1123dd4421ab6229
Laurindosnt <32465756+Laurindosnt@users.noreply.github.com>
2017-10-16 17:38:33 -0200

    Add files via upload

--- Exlis4.hs
+++ Exlis4.hs
@@ -1,39 +1,18 @@
---Exercício 04: Utilizando a lista anterior, calcule a soma dos números de Fibonacci pares dos valores que não excedem 4.000.000. (Project Euler 2)
+--Laurindo dos Santos - RA: 131710114
+--Exercício 04: 
+--Utilizando a lista anterior, calcule a soma dos números de Fibonacci pares dos valores que não excedem 4.000.000. (Project Euler 2)
 
 module Main where
 
-menor4M :: Integer -> Integer
-menor4M m
-  | m < 4000000 && ehpar == True = sumdr m
-  where 
-     ehpar = (m `rem` 2) == 0
-	 
-
-sumdr x
-  | x <= 9 = x
-  | otherwise = sumdr $ addupnums x
-
-  
-addupnums y = foldl (+) 0 $ map digito $ show y
-
-digito:: Char -> Int
-digito '1' = 1
-digito '2' = 2
-digito '3' = 3
-digito '4' = 4
-digito '5' = 5
-digito '6' = 6
-digito '7' = 7
-digito '8' = 8
-digito '9' = 9
-digito '0' = 0
-digito x = error "Número Inválido"
 
 main = do
 
    let listfibs = 0 : 1 : zipWith (+) listfibs (tail listfibs)
+   let lista = take 40 listfibs
+   let ehpar m = m `rem` 2 == 0 
+   let total = sum [n | n <- lista, n `mod` 2 == 0, (n < 4000000)]
 
-   let lista = take 10 listfibs
+   print (total)
 
 
 
